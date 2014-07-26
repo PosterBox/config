6bbe2fc4da4e9f50d4f01f3e6979c813b0767c40
Coney Wu <coney.wu@gmail.com>
2014-07-26 23:41:12 +0800

    update setup.sh, support standalone mode

--- iptables/setup.sh
+++ iptables/setup.sh
@@ -2,12 +2,31 @@
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -F
+function clear_all_rules {
+	iptables -t nat -F
+}
 
+function redirect_http {
+	iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.20.254:80
+}
 
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.20.254:80
-iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 192.168.20.254:53
+function redirect_dns {
+	iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 192.168.20.254:53
+}
 
-iptables -I POSTROUTING -t nat -o eth0 -j MASQUERADE
+function forward_packet {
+	iptables -I POSTROUTING -t nat -o eth0 -j MASQUERADE
+}
 
-iptables -t nat -L
+function list_rules {
+	iptables -t nat -L
+}
+
+clear_all_rules
+redirect_http
+if [[ $1 == "standalone" ]]; then
+	redirect_dns
+else
+	forward_packet
+fi
+list_rules
