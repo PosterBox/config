a57770aaed2b74f58945b755639f67c9bde68d05
Coney Wu <coney.wu@gmail.com>
2014-07-26 23:42:23 +0800

    bypass-daemon

---
+++ iptables/bypass-daemon.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PORT=3000
+
+
+function start {
+nc -kl $1 | while IFS=, read -a p; do
+	IP=${p[0]}
+	if check_ip $IP; then
+		process $IP
+	fi	
+done
+}
+
+function check_ip {
+	[[ -n `echo $1 | grep -P "(\d{1,3}\.){3}\d{1,3}"` ]]
+}
+
+function process {
+	echo add IP $1
+	./bypass.sh $1
+}
+
+
+start $PORT
